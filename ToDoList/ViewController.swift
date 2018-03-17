6fa61c507d9f7de1c00efa44afa3f68b229d11a1
selveus1 <fs031090@gmail.com>
2018-03-17 17:18:01 -0400

    Initial Commit

---
+++ ToDoList/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ToDoList
+//
+//  Created by Athena on 3/17/18.
+//  Copyright © 2018 Sheena Elveus. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
